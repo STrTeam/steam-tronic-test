b8112b628458755b880d3b3c0698e6514217923c
dryacile <dryacil85@gmail.com>
2014-09-12 22:49:45 +0200

    updated

---
+++ main/java/test/testbasic/testmod/lib/constants.java
@@ -0,0 +1,9 @@
+package test.testbasic.testmod.lib;
+
+public class constants {
+
+	public static final String MODID = "stronicTest";
+	public static final String MODNAME = "SteamTronicTest";
+	public static final String MODVERSION = "1.0";
+	
+}
---
+++ main/java/test/testbasic/testmod/stronicTest.java
@@ -0,0 +1,25 @@
+package test.testbasic.testmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "constants.MODID", name = "constants.MODNAME", version = "constants.MODVERSION")
+public class stronicTest {
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@Mod.EventHandler
+	public void posteInit(FMLPostInitializationEvent event) {
+		
+	}
+}
